e73862a263a376e2144f8f1fad778de3ec6fc200
Luis Carlos <cajas.luis@outlook.com>
2020-03-31 17:26:47 -0500

    Added main.cpp and source.cpp

---
+++ sandbox/Source.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <stdio.h>
+
+int main() {
+
+	int x;
+
+	x = 1 + 2;
+
+	return x;
+}
---
+++ sandbox/main.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <iostream>
+
+int main() {
+	
+	printf("Hello World\n");
+
+	system("pause");
+	return 0;
+}
